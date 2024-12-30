e7af1ddc6da1377c98f076dd3b903476c744dceb
Shryansh Parashar <Jaguar000212@gmail.com>
2024-12-30 20:06:02 +0530

    Finished 3nd Tool, Counter
    - Implementation completed, without any errors
    - Will work on bug improvements after testing
    - UI changes if necessary

--- app/src/main/java/com/jaguar/toolkit/data/ToolList.java
+++ app/src/main/java/com/jaguar/toolkit/data/ToolList.java
@@ -3,16 +3,17 @@ package com.jaguar.toolkit.data;
 import com.jaguar.toolkit.R;
 import com.jaguar.toolkit.objects.Tool;
 import com.jaguar.toolkit.tools.AverageCalculator;
+import com.jaguar.toolkit.tools.Counter;
 import com.jaguar.toolkit.tools.LoanCalculator;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ToolList {
-    static private List<Tool> tools = Arrays.asList(new Tool[]{
+    final static private List<Tool> tools = Arrays.asList(
             new Tool("Average Calculator", "Calculate average with some other interesting results.", R.mipmap.ic_tool_average, AverageCalculator.class),
             new Tool("Interest Calculator", "Calculates both simple and compound interests.", R.mipmap.ic_tool_loan, LoanCalculator.class),
-    });
+            new Tool("Counter", "A simple counter to increment, decrement and reset.", R.mipmap.ic_tool_counter, Counter.class));
 
     public static List<Tool> getTools() {
         return tools;
